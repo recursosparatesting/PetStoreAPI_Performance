8c25bc033ae4047e853a7bd7afe2dafe09cc1dc7
Maria Enith Franco <recursosparatesting@gmail.com>
2025-12-13 17:03:01 -0500

    Pruebas performance con Jmeter - MEF

--- stop_sar_monitoring.sh
+++ stop_sar_monitoring.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
-# Detiene el proceso de sar
+# 1. Detiene el proceso de sar
 SAR_PID=$(cat sar_pid.txt)
 echo "Deteniendo sar (PID: $SAR_PID)..."
-kill $SAR_PID
+# Usamos SIGTERM (15) para permitir que el proceso termine limpiamente
+kill -15 $SAR_PID
 
-# Convierte el archivo binario de sar a CSV
-# -d: Formato delimitado (CSV)
-# -A: Todas las métricas
+# Esperar un momento (vital) para que sar escriba los datos binarios finales
+sleep 5
+
+# 2. Convierte el archivo binario de sar a CSV
+# sadf necesita el path del archivo binario (-F sar.bin) y la salida (-d)
 sadf -d sar.bin -- -A > sar_metrics.csv
 echo "Métricas de sar guardadas en sar_metrics.csv"
