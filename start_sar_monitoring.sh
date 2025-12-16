236b39ec5c9f4eec97a2bd1e24abf4e4360540f0
Maria Enith Franco <recursosparatesting@gmail.com>
2025-12-16 11:55:17 -0500

    Despliegue de la prueba - MEF

--- start_sar_monitoring.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Crea un archivo binario para sar y lo ejecuta en segundo plano
-# -o: Guarda la salida en formato binario (sa.bin)
-# 5 0: Recolección cada 5 segundos (0 significa hasta que se detenga)
-sar -o sar.bin -A 5 0 > /dev/null 2>&1 &
-echo $! > sar_pid.txt
-echo "sar iniciado en background con PID $(cat sar_pid.txt)"
